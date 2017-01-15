a471f100a8546bc5c9e3a6e2004660ef89ce4485
emadahmed97 <emad1997@gmail.com>
2017-01-15 15:07:16 -0500

    image support

--- js/template.js
+++ js/template.js
@@ -10,7 +10,7 @@
 (function($){
 	$(document).ready(function(){
 
-		$(".banner-image").backstretch('images/IMG_2519.jpg');
+		$(".banner-image").backstretch('../images/IMG_2519.jpg');
 
 		// Fixed header
 		//-----------------------------------------------
